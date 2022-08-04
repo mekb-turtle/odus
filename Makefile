be809f65807d80572fdcf288895f004c700de4fe
mekb the turtle <mekbgithub@icloud.com>
2022-08-05 00:49:27 +1000

    login flag, and password input

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=cc
 CFLAGS=-Wall -O2
-LFLAGS=-lm
+LFLAGS=-lm -lcrypt
 
 OBJS=main.o
 
--- main.c
+++ main.c
@@ -6,9 +6,7 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <grp.h>
-#if HAVE_SHADOW_H
 #include <shadow.h>
-#endif
 #define ODUS_GROUP "odus"
 #define eprintf(...) fprintf(stderr, __VA_ARGS__)
 #define strerr strerror(errno)
@@ -16,6 +14,7 @@
 int usage(char *argv0) {
 	eprintf("\
 Usage: %s [options] [command] [argv]...\n\
+	-l --login : set HOME and USER environment variables\n\
 	-u --user [user] : the user to run as, defaults to UID 0, which is usually root\n\
 		if numeric, it'll find a user with the UID, otherwise with that name\n\
 ", argv0);
@@ -31,6 +30,7 @@ bool to_name_or_id(char *str, long *out) {
 	if (numeric) *out = atol(str);
 	return numeric;
 }
+#ifndef DEBUG
 bool getgrouplist_(char *user, gid_t group, gid_t **groups_, int *ngroups_) {
 	// getgrouplist but we don't know the size yet
 	int cur_ngroups = 0;
@@ -49,6 +49,46 @@ bool getgrouplist_(char *user, gid_t group, gid_t **groups_, int *ngroups_) {
 	*ngroups_ = ngroups;
 	return 1;
 }
+bool password_check(uid_t my_uid) {
+	struct passwd *pw = getpwuid(my_uid);
+	if (errno) { eprintf("getpwuid: %s\n",        strerr); return 0; }
+	if (!pw)   { eprintf("Cannot find user %i\n", my_uid); return 0; }
+	char *p = pw->pw_passwd;
+	if (p[0] == 'x' && p[1] == '\0') {
+		errno = 0;
+		struct spwd *spw = getspnam(pw->pw_name);
+		if (errno) { eprintf("getspnam: %s\n",          strerr);      return 0; }
+		if (!spw)  { eprintf("Cannot find shadow %s\n", pw->pw_name); return 0; }
+		p = spw->sp_pwdp;
+	}
+	if (p[0] == '!' || p[0] == '*') {
+		eprintf("Permission denied\n");
+		return 0;
+	} else if (p[0] == '\0') {
+		return 1;
+	}
+	char *prompt = malloc(100);
+	sprintf(prompt, "Password for %s: ", pw->pw_name);
+	for (int i = 0; i < 3; ++i) {
+		errno = 0;
+		char *input = getpass(prompt); // temporary, this function is obsolete
+		if (errno) { eprintf("getpass: %s\n", strerr); return 0; }
+		errno = 0;
+		char *c = crypt(input, p);
+		if (errno) { eprintf("crypt: %s\n", strerr); return 0; }
+		if (!c) { eprintf("crypt\n"); return 0; }
+		if (strcmp(p, c) == 0) {
+			return 1;
+		} else {
+			eprintf("Invalid password\n");
+			if (input[0] == '\0') return 0;
+			continue;
+		}
+	}
+	eprintf("Permission denied\n");
+	return 0;
+}
+#endif
 int main(int argc, char *argv[]) {
 #define INVALID return usage(argv[0]);
 #ifndef DEBUG
@@ -77,7 +117,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	if (!has_group) {
-		eprintf("`%s` is not in the `%s` group\n", pwd_->pw_name, ODUS_GROUP);
+		eprintf("%s is not in group %s\n", pwd_->pw_name, ODUS_GROUP);
 		return 1;
 	}
 #endif
@@ -86,16 +126,21 @@ int main(int argc, char *argv[]) {
 	char *cmd_argv[argc];
 	int cmd_argc = 0;
 	bool user_flag = 0;
+	bool login_flag = 0;
 	bool flag_done = 0;
 	for (int i = 1; i < argc; ++i) {
 		if (user_flag) {
 			user_str = argv[i];
 			user_flag = 0;
 		} else if (argv[i][0] == '-' && argv[i][1] != '\0' && !flag_done) {
-			if (argv[i][1] == '-' && argv[i][2] == '\0') flag_done = 1; // -- denotes end of flags
-			else if (strcmp(argv[i], "-u") == 0 || strcmp(argv[i], "--user") == 0) {
+			if (argv[i][1] == '-' && argv[i][2] == '\0') flag_done = 1; else // -- denotes end of flags
+			if (strcmp(argv[i], "-u") == 0 || strcmp(argv[i], "--user") == 0) {
 				if (user_str) INVALID;
 				user_flag = 1;
+			} else
+			if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--login") == 0) {
+				if (login_flag) INVALID;
+				login_flag = 1;
 			} else
 			INVALID;
 		} else {
@@ -103,6 +148,7 @@ int main(int argc, char *argv[]) {
 			cmd_argv[cmd_argc++] = argv[i]; // add to argv
 		}
 	}
+	if (user_flag) INVALID;
 	if (cmd_argc <= 0) INVALID;
 	cmd_argv[cmd_argc] = NULL;
 	bool user_numeric = 1;
@@ -130,10 +176,19 @@ int main(int argc, char *argv[]) {
 		printf("argv[%i]: %s\n", i, cmd_argv[i]);
 	}
 #else
-	if (setuid(pwd->pw_uid)  != 0) { eprintf("setuid: %s\n",  strerr); return errno; }
-	if (setgid(pwd->pw_gid)  != 0) { eprintf("setgid: %s\n",  strerr); return errno; }
-	if (seteuid(pwd->pw_uid) != 0) { eprintf("seteuid: %s\n", strerr); return errno; }
-	if (setegid(pwd->pw_gid) != 0) { eprintf("setegid: %s\n", strerr); return errno; }
+	errno = 0;
+	if (password_check(my_uid) != 1) {
+		if (errno) return errno;
+		return 1;
+	}
+	errno = 0; if (setuid(pwd->pw_uid)  != 0) { eprintf("setuid: %s\n",  strerr); return errno; }
+	errno = 0; if (setgid(pwd->pw_gid)  != 0) { eprintf("setgid: %s\n",  strerr); return errno; }
+	errno = 0; if (seteuid(pwd->pw_uid) != 0) { eprintf("seteuid: %s\n", strerr); return errno; }
+	errno = 0; if (setegid(pwd->pw_gid) != 0) { eprintf("setegid: %s\n", strerr); return errno; }
+	if (login_flag) {
+		setenv("HOME", pwd->pw_dir, 1);
+		setenv("USER", pwd->pw_name, 1);
+	}
 	execvp(cmd_argv[0], cmd_argv);
 	eprintf("execvp: %s\n", strerr);
 #endif
